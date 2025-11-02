e665e27e698c5e99a41df78b5baefb3c9590d586
mohammedayman777 <ma75073377@gmail.com>
2025-11-03 00:47:41 +0200

    Revert "MainComponent.cpp"

--- MainComponent.cpp
+++ MainComponent.cpp
@@ -1,15 +1,19 @@
 #include "MainComponent.h"
 
 MainComponent::MainComponent()
-    : playerGUI(playerAudio), playerGUI2(playerAudio2)
+    : playerGUI(playerAudio), 
+      playerGUI2(playerAudio2)
 {
     addAndMakeVisible(playerGUI);
     addAndMakeVisible(playerGUI2);
-    setSize(1200, 500);
+    setSize(1200, 400);
     setAudioChannels(0, 2);
 }
 
-MainComponent::~MainComponent() { shutdownAudio(); }
+MainComponent::~MainComponent()
+{
+    shutdownAudio();
+}
 
 void MainComponent::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
 {
@@ -20,8 +24,10 @@ void MainComponent::prepareToPlay(int samplesPerBlockExpected, double sampleRate
 void MainComponent::getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill)
 {
     bufferToFill.clearActiveBufferRegion();
+
     juce::AudioSourceChannelInfo info1(bufferToFill);
     playerAudio.getNextAudioBlock(info1);
+
     juce::AudioSourceChannelInfo info2(bufferToFill);
     playerAudio2.getNextAudioBlock(info2);
 }
@@ -29,16 +35,20 @@ void MainComponent::getNextAudioBlock(const juce::AudioSourceChannelInfo& buffer
 void MainComponent::releaseResources()
 {
     playerAudio.releaseResources();
-    playerAudio2.releaseResources();
 }
 
-void MainComponent::paint(juce::Graphics& g) { g.fillAll(juce::Colours::darkgrey); }
+void MainComponent::paint(juce::Graphics& g)
+{
+    g.fillAll(juce::Colours::darkgrey);
+}
 
 void MainComponent::resized()
 {
     auto area = getLocalBounds().reduced(10);
+
     auto leftArea = area.removeFromLeft(area.getWidth() / 2).reduced(5);
     auto rightArea = area.reduced(5);
+
     playerGUI.setBounds(leftArea);
     playerGUI2.setBounds(rightArea);
 }
